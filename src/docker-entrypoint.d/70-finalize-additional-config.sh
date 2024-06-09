e2beb30531cfc78ddb583382efbe84874558587a
Kim Oliver Drechsel <kim@drechsel.xyz>
2024-06-09 12:12:53 +0200

    Fix FIRST_INSTALL flag file persisting after container recreation
    Signed-off-by: Kim Oliver Drechsel <kim@drechsel.xyz>

---
+++ src/docker-entrypoint.d/70-finalize-additional-config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Remove empty lines in file
+sed -iE '/^$/d' "${WEB_ROOT}"/typo3conf/system/additional.php
+
+# Mark as first install if not initialized yet
+INIT_FLAG_FILE="${WEB_ROOT}"/typo3conf/.initialized
+[ ! -f "$INIT_FLAG_FILE" ] && touch "${WEB_ROOT}"/FIRST_INSTALL
+touch "$INIT_FLAG_FILE"
--- src/docker-entrypoint.d/90-adjust-typo3-permissions.sh
+++ src/docker-entrypoint.d/90-adjust-typo3-permissions.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Remove empty lines in file
-sed -iE '/^$/d' "${WEB_ROOT}"/typo3conf/system/additional.php
-
 # Adjust permissions
 for dir in fileadmin typo3conf typo3temp uploads;
 do
